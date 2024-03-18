00e27625bb089e6159c72b225a967ba0fa31e497
R-HYTE <polycarpwathuta39@gmail.com>
2024-03-18 16:55:38 +0300

    Create duka dashboard

---
+++ app.js
@@ -0,0 +1,5 @@
+// Function to toggle navigation menu
+function toggleMenu() {
+  const menu = document.querySelector('.navigation-menu');
+  menu.classList.toggle('active'); // Toggle the 'active' class
+}
