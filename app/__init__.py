afd64a1c763b29df2937e4c126e0d9b4ca56cb98
R-HYTE <polycarpwathuta39@gmail.com>
2024-03-25 17:20:45 +0300

    Add routes to my server

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,27 @@
+from flask import render_template
 from flask import jsonify
 from app import app
 from app.models import Product
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/my_shops')
+def my_shops():
+    # Placeholder function for my_shops endpoint
+    return render_template('my_shops.html')
+
+@app.route('/sell')
+def sell():
+    # Placeholder function for sell endpoint
+    return render_template('sell.html')
+
+@app.route('/stock')
+def stock():
+    # Placeholder function for sell endpoint
+    return render_template('stock.html')
 
 @app.route('/products', methods=['GET'])
 def get_products():
